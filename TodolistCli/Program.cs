2367e2e92a753631180b7a64ea1e4f2d3e3fb9b2
Sergey Egorov <xxxxx@yyy>
2022-10-25 18:57:02 +0300

    init new projects

---
+++ TodolistCli/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("I want to be a programmer because it is very interesting");
+Console.WriteLine("You can create something new");
+Console.WriteLine("You can realise your idea and idea other people");
